534937d9dd852b03a40fb33a0f9ab3546da3ddfd
Ivan Storck <ivan@ivanstorck.com>
2020-08-19 16:38:53 -0700

    make it weatherized

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import * as React from 'react';
 import { hot } from "react-hot-loader/root";
 
 interface Props {
-   name:
+   station:
     string
 }
 
 class App extends React.Component<Props> {
   render() {
-    const { name } = this.props;
+    const { station } = this.props;
     return (
       <>
         <h1 className="text-4xl text-white bg-black">
-          Hello {name}
+          Weather for {station}
         </h1>
       </>
     );
